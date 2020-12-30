46b656ab68f726f08cb3f92065b7e6eb1a46f178
sparkidea25 <michaeldaralola123@gmail.com>
2020-12-30 16:42:06 +0100

    newly-added

--- main.go
+++ main.go
@@ -5,29 +5,40 @@ import (
 	"strings"
 )
 
+const (
+	A = "hello world"
+	B = "Who"
+)
+
+func check(word string) {
+	aContains := false
+	bContains := false
+
+	fmt.Print(word)
+	word = strings.ToLower(word)
+
+	if strings.Contains(strings.ToLower(A), word) {
+		aContains = true
+	}
+
+	if strings.Contains(strings.ToLower(B), word) {
+		aContains = true
+	}
+
+	if aContains && !bContains {
+		fmt.Println(" exists in A and not B")
+	}
+
+	if !aContains && bContains {
+		fmt.Println(" exists in B and not A")
+	}
+
+	if !aContains && !bContains {
+		fmt.Println(" doesnt exist in both A and B")
+	}
+}
+
 func main() {
-	var a = "welcome to my world of Golang"
-	var b = "welcome to the world of Fintech and Blockchain"
-
-	input := "world"
-	inpute := "Fintech"
-
-	if strings.Contains(a, input) {
-		fmt.Println("welcome a")
-	}
-	if strings.Contains(b, input) {
-		fmt.Println("Welcome b")
-	}
-
-	if strings.Contains(a, inpute) {
-		fmt.Println("Welcome")
-	} else {
-		fmt.Println("No it doesnt")
-	}
-
-	if strings.Contains(b, inpute) {
-		fmt.Println("welcome")
-	} else {
-		fmt.Println("no it doesnt")
-	}
+	check("Hello")
+	check("Who")
 }
